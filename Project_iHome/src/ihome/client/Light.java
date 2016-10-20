7ddb92f8392ab7e069de6477ebc767363866b948
Lisette <Lisette van Leeuwen>
2016-10-20 11:01:41 +0200

    some work on light including error

--- Project_iHome/src/ihome/client/Light.java
+++ Project_iHome/src/ihome/client/Light.java
@@ -3,16 +3,22 @@ package ihome.client;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+import org.apache.avro.AvroRemoteException;
+import org.apache.avro.ipc.SaslSocketServer;
 import org.apache.avro.ipc.SaslSocketTransceiver;
+import org.apache.avro.ipc.Server;
 import org.apache.avro.ipc.Transceiver;
 import org.apache.avro.ipc.specific.SpecificRequestor;
+import org.apache.avro.ipc.specific.SpecificResponder;
 import org.json.JSONObject;
 
 import ihome.proto.serverside.ServerProto;
 import ihome.server.Controller;
+import ihome.proto.lightside.LightProto;
 
-public class Light {
+public class Light implements LightProto {
 	
+	private Server server = null;
 	private Controller controller = new Controller();
 	private static Transceiver light;
 	private static ServerProto proxy;
@@ -42,12 +48,19 @@ public class Light {
 			ID = json.getInt("UID");
 			name = "light" + nextName++;
 			System.out.println("name: " + name + " ID: " + ID);
+			runServer();
 		} catch (Exception e){
 			System.out.println(e.getMessage());
 		}
 	}
 	
-	public void switch_state() {
+	@Override
+	public CharSequence send_state() throws AvroRemoteException {
+		return "{\"state\" : \"" + state + "\"}";
+	}
+
+	@Override
+	public CharSequence switch_state() throws AvroRemoteException {
 		if (state == "on") {
 			state = "off";
 			System.out.println("Light " + name + " with ID " + ID + " is switched off.");
@@ -55,10 +68,30 @@ public class Light {
 			state = "on";
 			System.out.println("Light " + name + " with ID " + ID + " is switched on.");
 		}
+		return "{\"Error\" : NULL}";
 	}
 	
-	public String get_state() {
-		return state;
+	public void runServer() {
+		try
+		{
+			server = new SaslSocketServer(new SpecificResponder(ServerProto.class,
+					new Controller()), new InetSocketAddress(6790+ID));
+		}catch (IOException e){
+			System.err.println("[error] failed to start server");
+			e.printStackTrace(System.err);
+			System.exit(1);
+
+		}
+		server.start();
+	}
+	
+	public void stopServer() {
+		try {
+			server.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -66,14 +99,11 @@ public class Light {
 		Light.connect_to_server();
 		Light myLight = new Light();
 		myLight.add_to_house();
-		while (true) {
-			// execute actions from command line
-			/*
-			 * Possible actions:
-			 * 		Switch state
-			 * 		Ask for current state
-			 */
+		while(true){
+			int a;
 		}
+		
+		//myLight.stopServer();
 	}
 
 }
